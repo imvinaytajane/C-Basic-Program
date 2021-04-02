4656812baa6ae066fd244976ad881ed10b6664aa
Vinay <imvinaytajane@gmail.com>
2021-04-02 14:48:10 +0530

    Made  Rectanglr Area Program

---
+++ RectangleAreaFinder.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main ()
+{
+    int a , b ;
+
+    printf("Input Length of Rectangle:-");
+    scanf("%d",&a);
+
+    printf("Input Breadth of Rectangle:-");
+    scanf("%d",&b);
+
+
+    printf( "Area of Rectangle is :%d" ,a * b);
+
+    return 0 ;
+}
