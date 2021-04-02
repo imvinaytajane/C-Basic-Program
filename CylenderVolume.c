6fea916e175fd58a03ccfc781f8917b779bc0d79
Vinay <imvinaytajane@gmail.com>
2021-04-02 16:41:03 +0530

    redu previous

--- CylenderVolume.c
+++ CylenderVolume.c
@@ -8,15 +8,15 @@ int main()
 
     p=3.14;
 
-    printf("Input Radius of Circle/Base :-");
+    printf("Input Radius of Circle/Base :-\n");
     scanf( "%f",&r);
 
-    printf( "Area of circle/Base is %f :", p*r*r);
+    printf( "Area of circle/Base is %f \n:", p*r*r);
 
-    printf("Input Height of Cylender:-");
+    printf("Input Height of Cylender:- \n");
     scanf("%f",&h);
 
-    printf("Volume of cylender is %f",p*r*r*h);
+    printf("Volume of cylender is %f \n",p*r*r*h);
 
 
 
