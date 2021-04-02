bd7c35adc8769b7f58268ec95358e2533b4e1522
Vinay <imvinaytajane@gmail.com>
2021-04-02 12:25:55 +0530

    resolved error

---
+++ calculator01.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main ()
+ {
+
+
+    int a;
+    int b;
+
+    printf("Enter The First Number:-");
+    scanf("%d" ,&a);
+
+
+     printf("Enter The Second Number:-");
+    scanf("%d" ,&b);
+
+    int c=a+b ;
+
+
+    printf("Your Sum is :%d",c );
+
+    return 0;
+
+
+
+
+}
