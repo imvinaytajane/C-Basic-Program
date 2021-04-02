c9310f265f79776d29a4cc8f9363d381f71f0122
Vinay <imvinaytajane@gmail.com>
2021-04-02 16:39:18 +0530

    circle area into volume

--- CylenderVolume.c
+++ CylenderVolume.c
@@ -2,16 +2,23 @@
 
 int main()
 {
-    int r ,h;
+    float r ,h;
 
     float p;
 
     p=3.14;
 
     printf("Input Radius of Circle/Base :-");
-    scanf( "%d",&r);
+    scanf( "%f",&r);
 
     printf( "Area of circle/Base is %f :", p*r*r);
+
+    printf("Input Height of Cylender:-");
+    scanf("%f",&h);
+
+    printf("Volume of cylender is %f",p*r*r*h);
+
+
 
     return 0;
 
