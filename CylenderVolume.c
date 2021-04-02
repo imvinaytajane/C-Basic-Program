d1e1255bd837b6a96fd69ba0e592827007329375
Vinay <imvinaytajane@gmail.com>
2021-04-02 15:07:58 +0530

    Program Circle Area Finder

---
+++ CylenderVolume.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int r ,h;
+
+    float p;
+
+    p=3.14;
+
+    printf("Input Radius of Circle/Base :-");
+    scanf( "%d",&r);
+
+    printf( "Area of circle/Base is %f :", p*r*r);
+
+    return 0;
+
+
+}
