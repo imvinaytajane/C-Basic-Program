e2cc7011d997294f557fac100d2ef25c6973b06a
Vinay <imvinaytajane@gmail.com>
2021-04-05 13:49:40 +0530

    Made number guess game

---
+++ NumberGuessGame.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number, guess, nguesses=1;
+    srand(time(0));
+    number = rand() % 100 + 1;
+
+    do
+    {
+        printf("Guess the Number Between 1 to 100\n");
+        scanf("%d", &guess);
+
+        if (guess > number)
+        {
+            printf("Lower Number Please!\n");
+        }
+
+        else if (guess < number)
+        {
+            printf("Greater Number Please!\n");
+        }
+
+        else
+        {
+            printf("You Guessed in %d Attempts\n", nguesses);
+        }
+
+        nguesses++;
+
+    } while (guess != number);
+
+    return 0;
+}
